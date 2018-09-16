a9a7960858302054d53d5b7616b16e519a81dc52
denissurkov <surkov0denis@yandex.ru>
2018-09-16 23:21:02 +0500

    add new operations with Math lib

---
+++ HomeAssigments/src/lecture1_2/Calculator.java
@@ -0,0 +1,92 @@
+package lecture1_2;
+
+import java.util.Scanner;
+
+class Calculator {
+
+     void run() {
+            Scanner scan = new Scanner(System.in);
+
+            float a, b;
+            String action = " ";
+
+            System.out.println("Calculator :) \n");
+            // TODO: print to user list of available operations
+
+            while (!action.equals("exit")) {
+                System.out.println("Input first number: ");
+                a = scan.nextFloat();
+
+                System.out.println("Input operation: ");
+                action = scan.next();
+
+                System.out.println("Input second number (if second num is unneeded just pass 0): ");
+                b = scan.nextFloat();
+
+                System.out.println(getResultOfOperation(action, a, b));
+            }
+        }
+
+    private String getResultOfOperation(String operation, float a, float b){
+            float result = 0f;
+            String message;
+            boolean incorrect = false;
+
+            switch (operation){
+                case "+":
+                    result = a + b;
+                    break;
+                case "-":
+                    result = a - b;
+                    break;
+                case "/":
+                    result = a / b;
+                    break;
+                case "%":
+                    result = a % b;
+                    break;
+                case "*":
+                    result = a * b;
+                    break;
+                case "^":
+                    result = Math.round(Math.pow(a, b));
+                    break;
+                case "!":
+                    result = getFactorial(a);
+                    break;
+                case "log":
+                    result = Math.round(Math.log((double) a));
+                    break;
+                case "sin":
+                    result = Math.round(Math.sin((double) a));
+                    break;
+                case "cos":
+                    result = Math.round(Math.cos((double) a));
+                    break;
+                case "acos":
+                    result = Math.round(Math.acos((double) a));
+                    break;
+
+                default:
+                    incorrect = true;
+                    break;
+            }
+
+            if (!incorrect) {
+                message = "a " + operation + " (b) = " + result;
+            } else {
+                message = "\tUnknown operation with symbol " + operation + ".\n\tPlease try again.\n\n";
+            }
+
+            return message;
+        }
+
+    public static float getFactorial(float a){
+            if (a <= 0 + Float.MIN_NORMAL || a <= 0 - Float.MIN_NORMAL){
+                return 1f;
+            }
+            return a * (getFactorial(a - 1));
+        }
+}
+
+
---
+++ HomeAssigments/src/lecture1_2/Main.java
@@ -0,0 +1,9 @@
+package lecture1_2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Calculator().run();
+    }
+
+}
